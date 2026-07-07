da14c36845071bd1b0958fbc243db2649a5efb46
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-07-07 19:20:55 +0530

    Create custom-select.js

---
+++ js/custom-select.js
@@ -0,0 +1,68 @@
+document.querySelectorAll("select").forEach(select => {
+
+    const wrapper = document.createElement("div");
+    wrapper.className = "custom-select";
+
+    select.parentNode.insertBefore(wrapper, select);
+    wrapper.appendChild(select);
+
+    const selected = document.createElement("div");
+    selected.className = "select-selected";
+    selected.textContent = select.options[select.selectedIndex].textContent;
+
+    wrapper.appendChild(selected);
+
+    const options = document.createElement("div");
+    options.className = "select-items";
+
+    [...select.options].forEach((option,index)=>{
+
+        if(index===0) return;
+
+        const item=document.createElement("div");
+
+        item.textContent=option.textContent;
+
+        item.addEventListener("click",()=>{
+
+            select.selectedIndex=index;
+
+            selected.textContent=option.textContent;
+
+            wrapper.classList.remove("active");
+
+        });
+
+        options.appendChild(item);
+
+    });
+
+    wrapper.appendChild(options);
+
+    selected.addEventListener("click",(e)=>{
+
+        e.stopPropagation();
+
+        document.querySelectorAll(".custom-select")
+            .forEach(drop=>{
+                if(drop!==wrapper){
+                    drop.classList.remove("active");
+                }
+            });
+
+        wrapper.classList.toggle("active");
+
+    });
+
+});
+
+document.addEventListener("click",()=>{
+
+    document.querySelectorAll(".custom-select")
+        .forEach(drop=>{
+
+            drop.classList.remove("active");
+
+        });
+
+});
