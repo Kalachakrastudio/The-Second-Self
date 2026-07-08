5f0cdf09e5d097bc0e756fe5441dd99cb4f5b1d3
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-07-08 22:00:52 +0530

    Update ticket.js

--- js/ticket.js
+++ js/ticket.js
@@ -34,3 +34,42 @@ checkoutBtn.addEventListener("click", function(e){
     rzp.open();
 
 });
+function saveBooking(paymentId){
+
+const formData={
+
+sheet:"Tickets",
+
+paymentId:paymentId,
+
+name:name.value,
+
+mobile:mobile.value,
+
+email:email.value,
+
+ticketType:ticketType.value,
+
+quantity:quantity.value,
+
+amount:totalAmount.innerText
+
+};
+
+fetch(scriptURL,{
+
+method:"POST",
+
+body:JSON.stringify(formData)
+
+})
+
+.then(r=>r.json())
+
+.then(data=>{
+
+generateTicket(data.ticketId);
+
+});
+
+}
