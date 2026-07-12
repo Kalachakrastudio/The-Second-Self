775e378b7ae92eff8713b14be33626b332a11d3d
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-07-12 11:28:55 +0530

    Update performer.js

--- js/performer.js
+++ js/performer.js
@@ -64,6 +64,8 @@ form.addEventListener("submit", function (e) {
         successPopup.classList.add("show");
 
         form.reset();
+        resetCustomSelect("category");
+resetCustomSelect("eventDate");
 
     })
 
