155eadf9526d3240e25aa3f2551bf6abc03bbf43
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-07-12 15:05:28 +0530

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,5 +1,5 @@
 const SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbwLxdU3EA4r_gyZL_m9ywaOBjH90wB6eeIDk_FWnKlqRwG12-hgjhJGl5qfdKFeJBDf/exec";
+"https://script.google.com/macros/s/AKfycbxS4r7iNHtbIjfalJ7Iec8NyCQGmeuLmWrPzl6mP8b3ygWIJSBwCl6K13HkqjXEaxvn/exec";
 const html5QrCode =
 new Html5Qrcode("reader");
 
