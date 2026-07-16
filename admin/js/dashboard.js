3482ddd35e58ae4444ae6c5eb44e81950d556c99
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-07-16 20:57:07 +0530

    Update dashboard.js

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -1,1 +1,43 @@
+const contentArea=document.getElementById("contentArea");
 
+function loadPage(page,title){
+
+fetch(`modules/${page}.html`)
+
+.then(res=>res.text())
+
+.then(data=>{
+
+contentArea.innerHTML=data;
+
+document.getElementById("pageTitle").textContent=title;
+
+});
+
+}
+
+document.querySelectorAll(".sidebar-menu li")
+
+.forEach(item=>{
+
+item.addEventListener("click",()=>{
+
+document.querySelector(".sidebar-menu .active")
+
+?.classList.remove("active");
+
+item.classList.add("active");
+
+loadPage(
+
+item.dataset.page,
+
+item.dataset.title
+
+);
+
+});
+
+});
+
+loadPage("dashboard","Dashboard");
