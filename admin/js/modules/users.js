744f5202d9c9d0fa1eb0a397964b37bb12f2c3b7
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-07-26 14:26:18 +0530

    Update users.js

--- admin/js/modules/users.js
+++ admin/js/modules/users.js
@@ -51,13 +51,6 @@ function initUsers(){
         }
 
     };
-    setTimeout(()=>{
-
-    rebuildCustomSelect("userRole");
-
-    rebuildCustomSelect("userStatus");
-
-},100);
 
 }
 /*=========================================
@@ -89,6 +82,25 @@ document.getElementById("userStatus").value="Active";
 
 
 userModal.classList.add("show");
+
+
+setTimeout(()=>{
+
+    const role = document.getElementById("userRole");
+    const status = document.getElementById("userStatus");
+
+
+    if(role && !role.parentElement.classList.contains("custom-select")){
+        rebuildCustomSelect("userRole");
+    }
+
+
+    if(status && !status.parentElement.classList.contains("custom-select")){
+        rebuildCustomSelect("userStatus");
+    }
+
+
+},100);
 
 
 }
