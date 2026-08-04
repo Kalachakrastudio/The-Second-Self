ad38f83e506a2bdd10c4a2c299e77569f6c9e8e4
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-08-04 21:11:58 +0530

    Create audience.js

---
+++ admin/js/modules/audience.js
@@ -0,0 +1,17 @@
+const AUDIENCE_SCRIPT_URL = JUDGE_SCRIPT_URL;
+
+let audienceTicket = "";
+
+let audiencePerformers = [];
+
+let selectedAudiencePerformer = null;
+
+let selectedAudienceScore = 0;
+
+let selectedAudienceEvent = "";
+
+function initAudience(){
+
+    console.log("Audience Panel Loaded");
+
+}
